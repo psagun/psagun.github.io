febb39b7cbc9fedb531b061cedaceab8b6ef2c34
Sagun Pandey <twiggymarilyn00@gmail.com>
2021-03-19 01:16:01 -0700

    Contacts updated
    Links to social sites added

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Student", "Software Engineer", "Team Player", "Traveller",],
+        strings: ["Computer Science Student", "Software Engineer",],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Student", "Software Engineer", "Team Player", "Traveller",],
+        strings: ["Computer Science Student", "Software Engineer",],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
